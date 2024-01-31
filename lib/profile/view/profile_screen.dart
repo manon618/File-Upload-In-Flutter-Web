91c8d5c1c6334c651f4c3ee6816fbca812fe55b5
manon618 <manon2029@gmail.com>
2024-01-31 14:39:45 +0700

    fix

--- lib/profile/view/profile_screen.dart
+++ lib/profile/view/profile_screen.dart
@@ -14,7 +14,6 @@ class ProfileScreen extends StatefulWidget {
 
 class _ProfileScreenState extends State<ProfileScreen> {
   List<PlatformFile>? _paths;
-  final formKey = GlobalKey<FormState>();
 
   void pickFiles() async {
     try {
@@ -41,223 +40,173 @@ class _ProfileScreenState extends State<ProfileScreen> {
   }
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'eiMaths',
-                style: TextStyle(color: Colors.white, fontSize: 16.0),
-              ),
-              Text(
-                'Study Time Tracking',
-                style: TextStyle(color: Colors.white, fontSize: 14.0),
-              )
-            ],
-          ),
-          leading: const Padding(
-            padding: EdgeInsets.all(7.0),
-            child: CircleAvatar(
-              backgroundImage: AssetImage('assets/images/logo.png'),
-            ),
-          ),
-          actions: const <Widget>[Icon(Icons.more_vert)],
-        ),
-        body: SafeArea(
-          child: Container(
-            padding: const EdgeInsets.all(24),
-            child: Form(
-              key: formKey,
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    /* buildCentre(),
-                    const SizedBox(height: 16),
-                    buildStudent(),
-                    const SizedBox(height: 16),
-                    buildSubject(),
-                    const SizedBox(height: 16),
-                    buildTerm(),
-                    const SizedBox(height: 16),
-                    buildBook(),
-                    const SizedBox(height: 16),
-                    buildDate(),
-                    const SizedBox(height: 16),
-                    buildTime(),
-                    const SizedBox(height: 16),
-                    /* buildHour(),
-                    const SizedBox(height: 16), */
-                    buildeTime(),
-                    const SizedBox(height: 16),
-                    buildTextArea(),
-                    const SizedBox(height: 16),
-                    buildCamera(context),
-                    const SizedBox(height: 16),
-                    buildsignpad(),
-                    const SizedBox(height: 16),
-                    buildClear(context),
-                    const SizedBox(height: 12),
-                    buildSubmit(context) */
-                    buildPhoto(context)
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
-      );
-
-  Widget buildPhoto(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.blueGrey.shade200,
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Colors.blueGrey.shade800,
-              borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(
-                  20.00,
-                ),
-                bottomRight: Radius.circular(
-                  20.00,
-                ),
-              ),
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: 132.00,
-                  width: 327.00,
-                  margin: const EdgeInsets.only(
-                    left: 10.00,
-                    top: 24.00,
-                    right: 10.00,
-                  ),
-                  child: Stack(
-                    alignment: Alignment.bottomRight,
-                    children: [
-                      _paths != null
-                          ? Align(
-                              alignment: Alignment.bottomCenter,
-                              child: Container(
-                                height: 100.00,
-                                width: 100.00,
-                                margin: const EdgeInsets.only(
-                                  left: 113.00,
-                                  top: 10.00,
-                                  right: 113.00,
-                                ),
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(
-                                      50.00,
-                                    ),
-                                    image: DecorationImage(
-                                        image:
-                                            MemoryImage(_paths!.first.bytes!))),
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.blueGrey.shade200,
+        body: Column(
+          children: [
+            Expanded(
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: SingleChildScrollView(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blueGrey.shade200,
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: Colors.blueGrey.shade800,
+                            borderRadius: const BorderRadius.only(
+                              bottomLeft: Radius.circular(
+                                20.00,
                               ),
-                            )
-                          : Align(
-                              alignment: Alignment.bottomCenter,
-                              child: Container(
-                                height: 100.00,
-                                width: 100.00,
-                                margin: const EdgeInsets.only(
-                                  left: 113.00,
-                                  top: 10.00,
-                                  right: 113.00,
-                                ),
-                                decoration: BoxDecoration(
-                                    color: Colors.grey.shade400,
-                                    borderRadius: BorderRadius.circular(
-                                      50.00,
-                                    ),
-                                    image: const DecorationImage(
-                                        image: AssetImage(
-                                            'assets/images/image_not_found.png'))),
+                              bottomRight: Radius.circular(
+                                20.00,
                               ),
-                            ),
-                      InkWell(
-                        onTap: pickFiles,
-                        child: Align(
-                          alignment: Alignment.bottomCenter,
-                          child: Container(
-                            height: 30.00,
-                            width: 30.00,
-                            margin: const EdgeInsets.only(
-                              left: 183.00,
-                              top: 10.00,
-                              right: 113.00,
-                            ),
-                            decoration: BoxDecoration(
-                              color: Colors.white70,
-                              borderRadius: BorderRadius.circular(
-                                5.00,
-                              ),
-                            ),
-                            child: const Icon(
-                              Icons.camera_alt_outlined,
-                              size: 20,
-                              color: Colors.black,
                             ),
                           ),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                height: 132.00,
+                                width: 327.00,
+                                margin: const EdgeInsets.only(
+                                  left: 10.00,
+                                  top: 24.00,
+                                  right: 10.00,
+                                ),
+                                child: Stack(
+                                  alignment: Alignment.bottomRight,
+                                  children: [
+                                    _paths != null
+                                        ? Align(
+                                            alignment: Alignment.bottomCenter,
+                                            child: Container(
+                                              height: 100.00,
+                                              width: 100.00,
+                                              margin: const EdgeInsets.only(
+                                                left: 113.00,
+                                                top: 10.00,
+                                                right: 113.00,
+                                              ),
+                                              decoration: BoxDecoration(
+                                                  borderRadius:
+                                                      BorderRadius.circular(
+                                                    50.00,
+                                                  ),
+                                                  image: DecorationImage(
+                                                      image: MemoryImage(_paths!
+                                                          .first.bytes!))),
+                                            ),
+                                          )
+                                        : Align(
+                                            alignment: Alignment.bottomCenter,
+                                            child: Container(
+                                              height: 100.00,
+                                              width: 100.00,
+                                              margin: const EdgeInsets.only(
+                                                left: 113.00,
+                                                top: 10.00,
+                                                right: 113.00,
+                                              ),
+                                              decoration: BoxDecoration(
+                                                  color: Colors.grey.shade400,
+                                                  borderRadius:
+                                                      BorderRadius.circular(
+                                                    50.00,
+                                                  ),
+                                                  image: const DecorationImage(
+                                                      image: AssetImage(
+                                                          'assets/images/image_not_found.png'))),
+                                            ),
+                                          ),
+                                    InkWell(
+                                      onTap: pickFiles,
+                                      child: Align(
+                                        alignment: Alignment.bottomCenter,
+                                        child: Container(
+                                          height: 30.00,
+                                          width: 30.00,
+                                          margin: const EdgeInsets.only(
+                                            left: 183.00,
+                                            top: 10.00,
+                                            right: 113.00,
+                                          ),
+                                          decoration: BoxDecoration(
+                                            color: Colors.white70,
+                                            borderRadius: BorderRadius.circular(
+                                              5.00,
+                                            ),
+                                          ),
+                                          child: const Icon(
+                                            Icons.camera_alt_outlined,
+                                            size: 20,
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                  left: 10.00,
+                                  top: 16.00,
+                                  right: 10.00,
+                                ),
+                                child: Text(
+                                  "Noah Arielken",
+                                  overflow: TextOverflow.ellipsis,
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                    color: Colors.grey.shade50,
+                                    fontSize: 20,
+                                    fontFamily: 'Ubuntu',
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                  left: 10.00,
+                                  top: 8.00,
+                                  right: 10.00,
+                                  bottom: 48.00,
+                                ),
+                                child: Text(
+                                  "Visual Communication Design",
+                                  overflow: TextOverflow.ellipsis,
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                    color: Colors.grey.shade50,
+                                    fontSize: 14,
+                                    fontFamily: 'Ubuntu',
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                    left: 10.00,
-                    top: 16.00,
-                    right: 10.00,
-                  ),
-                  child: Text(
-                    "Noah Arielken",
-                    overflow: TextOverflow.ellipsis,
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      color: Colors.grey.shade50,
-                      fontSize: 20,
-                      fontFamily: 'Ubuntu',
-                      fontWeight: FontWeight.w700,
+                      ],
                     ),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                    left: 10.00,
-                    top: 8.00,
-                    right: 10.00,
-                    bottom: 48.00,
-                  ),
-                  child: Text(
-                    "Visual Communication Design",
-                    overflow: TextOverflow.ellipsis,
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      color: Colors.grey.shade50,
-                      fontSize: 14,
-                      fontFamily: 'Ubuntu',
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
